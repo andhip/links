be8e3d88f6a884bdbeefcc9039b80556f49a0518
andhip <andhi.puspianto10@gmail.com>
2021-02-20 01:19:17 +0700

    update port

---
+++ assets/JS/script.js
@@ -0,0 +1,42 @@
+// document ready
+$(document).ready(() => {
+
+
+    $(document.getElementById("kontak")).on('click', function (event) {
+
+        if (this.hash !== "0") {
+
+            event.preventDefault();
+            // Store hash
+            var hash = this.hash;
+
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1500, 'easeInOutExpo', function () {
+
+                window.location.hash = hash;
+            });
+        }
+        // End if
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+
+});
