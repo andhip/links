b491444faabcd9d2621f421abe1f9ae48b29a41f
andhip <andhi.puspianto10@gmail.com>
2021-03-24 17:24:44 +0700

    update css

--- animate.js
+++ animate.js
@@ -1,25 +1,26 @@
 var animatePoints = function () {
-    var revealPoint = function () {
-        $(this).css({
-            opacity: 1,
-            transform: 'scaleX(1) translateY(0)'
-        });
-    };
+  var revealPoint = function () {
+    $(this).css({
+      opacity: 1,
+      transform: "scaleX(1) translateY(0)",
+    });
+  };
 
-    $.each($('.point'), revealPoint);
-
+  $.each($(".point"), revealPoint);
 };
 
 $(window).load(function () {
-    if ($(window).height() > 920) { //for index => 950
-        animatePoints();
+  if ($(window).height() > 920) {
+    //for index => 950
+    animatePoints();
+  }
+
+  var scrollDistance =
+    $(".selling-points").offset().top - $(window).height() + 280; //for index => 120
+
+  $(window).scroll(function (event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
-
-    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 280; //for index => 120
-
-    $(window).scroll(function (event) {
-        if ($(window).scrollTop() >= scrollDistance) {
-            animatePoints();
-        }
-    });
+  });
 });
