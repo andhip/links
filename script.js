131b0451b8666aba49f945a9005fd0e414b2127f
andhip <andhi.puspianto10@gmail.com>
2021-04-09 10:25:21 +0700

    modified m-query

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
 var typer = {
-	words: ['FRONT-END DEVELOPER', 'DESIGNER', 'BUILDER', 'FORWARD-THINKER'],
-	wordIndex: 0,
-	speed: 150,
-	nextWordWait: 1000,
-	html: {
-		word: document.querySelector('.myWord')
-	},
-	init: function init() {
-		this.typeLetter();
-	},
-	typeLetter: function typeLetter() {
-		var _this = this;
+  words: ["FRONT-END DEVELOPER", "DESIGNER", "BUILDER", "FORWARD-THINKER"],
+  wordIndex: 0,
+  speed: 150,
+  nextWordWait: 1000,
+  html: {
+    word: document.querySelector(".myWord"),
+  },
+  init: function init() {
+    this.typeLetter();
+  },
+  typeLetter: function typeLetter() {
+    var _this = this;
 
-		var timer = arguments.length <= 0 || arguments[0] === undefined ? this.speed : arguments[0];
+    var timer =
+      arguments.length <= 0 || arguments[0] === undefined
+        ? this.speed
+        : arguments[0];
 
-		setTimeout(function () {
-			var word = {
-				finished: _this.words[_this.wordIndex],
-				current: _this.readWord()
-			};
+    setTimeout(function () {
+      var word = {
+        finished: _this.words[_this.wordIndex],
+        current: _this.readWord(),
+      };
 
-			var nextLetterIndex = word.finished.length - word.current.length;
-			if (nextLetterIndex > 0) {
-				nextLetterIndex = word.finished.length - nextLetterIndex;
-				var nextLetter = word.finished[nextLetterIndex];
-				_this.html.word.innerHTML += nextLetter;
-				_this.typeLetter();
-				return;
-			}
-			_this.nextWord();
-		}, timer);
-	},
-	readWord: function readWord() {
-		return this.html.word.innerHTML;
-	},
-	clearWord: function clearWord() {
-		this.html.word.innerHTML = '';
-	},
-	nextWord: function nextWord() {
-		var _this2 = this;
+      var nextLetterIndex = word.finished.length - word.current.length;
+      if (nextLetterIndex > 0) {
+        nextLetterIndex = word.finished.length - nextLetterIndex;
+        var nextLetter = word.finished[nextLetterIndex];
+        _this.html.word.innerHTML += nextLetter;
+        _this.typeLetter();
+        return;
+      }
+      _this.nextWord();
+    }, timer);
+  },
+  readWord: function readWord() {
+    return this.html.word.innerHTML;
+  },
+  clearWord: function clearWord() {
+    this.html.word.innerHTML = "";
+  },
+  nextWord: function nextWord() {
+    var _this2 = this;
 
-		this.wordIndex++;
-		if (this.wordIndex == this.words.length) {
-			this.wordIndex = 0;
-		}
-		setTimeout(function () {
-			_this2.clearWord();
-			_this2.typeLetter(_this2.nextWordWait);
-		}, this.nextWordWait);
-	}
+    this.wordIndex++;
+    if (this.wordIndex == this.words.length) {
+      this.wordIndex = 0;
+    }
+    setTimeout(function () {
+      _this2.clearWord();
+      _this2.typeLetter(_this2.nextWordWait);
+    }, this.nextWordWait);
+  },
 };
 
 typer.init();
@@ -56,16 +59,19 @@ typer.init();
 console.log("© 2020 Andynhov All rights reserved");
 
 $(document).ready(() => {
-	window.dataLayer = window.dataLayer || [];
-	function gtag() {
-		dataLayer.push(arguments);
-	}
-	gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
 
-	gtag('config', 'UA-177429414-1');
-
+  gtag("config", "UA-177429414-1");
 });
 
-document.addEventListener("contextmenu", function(e){
+document.addEventListener(
+  "contextmenu",
+  function (e) {
     e.preventDefault();
-}, false);
+  },
+  false
+);
