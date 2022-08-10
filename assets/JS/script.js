13fb3de28303865cc49b9d242ea91ac92f486d24
andhip <andhi.puspianto10@gmail.com>
2022-08-10 16:20:59 +0700

    modified indx

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,49 +1,36 @@
-// document ready
-$(document).ready(() => {
-  $(document.getElementById("kontak")).on("click", function (event) {
-    if (this.hash !== "0") {
-      event.preventDefault();
-      // Store hash
-      var hash = this.hash;
+// ------------------------------------------------------------------------------ //
+// Live CLOCK
+// ---------------------------------------------------------
 
-      $("html,body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        1500,
-        "easeInOutExpo",
-        function () {
-          window.location.hash = hash;
-        }
-      );
-    }
-    // End if
-  });
+function updateTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  var t_str = hours + ":" + minutes + " ";
+  if (hours > 11) {
+    t_str += "PM";
+  } else {
+    t_str += "AM";
+  }
+  document.getElementById("mod__clock").innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
+// ------------------------------------------------------------------------------ //
+// Live CLOCK - STATEMENT
+// ---------------------------------------------------------
+var hour = new Date().getHours();
+let doo;
+if (hour < 8) {
+  doo = "making coffee";
+} else if (hour > 8 && hour <= 12) {
+  doo = "designing things";
+} else if (hour > 12 && hour <= 18) {
+  doo = "bringing ideas to life";
+} else {
+  doo = "dreaming";
+}
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-
-  $(".back-to-top").click(function () {
-    $("html, body").animate(
-      {
-        scrollTop: 0,
-      },
-      1500,
-      "easeInOutExpo"
-    );
-    return false;
-  });
-
-  // preloader
-  // $(document).ready(function () {
-  //   setTimeout(function () {
-  //     $("body").addClass("loaded");
-  //   }, 3000);
-  // });
-});
+document.getElementById("mod__doo").innerHTML = doo;
